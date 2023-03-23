0065577e9e04053b4f570f6bbdc91ab7f45350f1
JAtoms <joshua.nwokoye.g20@gmail.com>
2023-03-23 19:29:29 +0100

    Refactor

--- lib/helpers/global_assets.dart
+++ lib/helpers/global_assets.dart
@@ -9,11 +9,11 @@ class GlobalAssets {
   static const String peopleMobileView = 'assets/images/people_mobile_view.png';
   static const String peopleWebView = 'assets/images/people_web_view.png';
   static const String successCheck = 'assets/images/success_check.png';
+  static const String qwivaLogo = 'assets/images/qwiva_logo.png';
 
   // SVGs
   static const String instagram = 'assets/svg/instagram.svg';
   static const String linkedln = 'assets/svg/linkedln.svg';
   static const String twitter = 'assets/svg/twitter.svg';
   static const String logo = 'assets/svg/logo.svg';
-  static const String qwivaLogo = 'assets/svg/qwiva_logo.svg';
 }
